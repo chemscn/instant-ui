4546f4b03e20d2338c3972d2950efd56d8726a0a
unknown <chemdev233@gmail.com>
2022-02-18 21:25:29 -0500

    attempt one on running lint-staged

--- projects/instant-ui-library/src/lib/root/instant-ui-library.module.ts
+++ projects/instant-ui-library/src/lib/root/instant-ui-library.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { InstantUiLibraryComponent } from './instant-ui-library.component';
 
-
-
 @NgModule({
-  declarations: [
-    InstantUiLibraryComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    InstantUiLibraryComponent
-  ]
+  declarations: [InstantUiLibraryComponent],
+  imports: [],
+  exports: [InstantUiLibraryComponent],
 })
-export class InstantUiLibraryModule { }
+export class InstantUiLibraryModule {}
