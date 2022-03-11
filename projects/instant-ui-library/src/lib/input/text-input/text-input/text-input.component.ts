dfc33ca0d3e870278ef2c8875afce8c49647a656
Jacarder <jonandrewcarder@gmail.com>
2022-03-10 19:45:01 -0500

    Initial files for text-input

---
+++ projects/instant-ui-library/src/lib/input/text-input/text-input/text-input.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'iu-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TextInputComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
