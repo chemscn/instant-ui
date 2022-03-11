361536a96f9bfbd32710ec391750d7f20511ab13
Jacarder <jonandrewcarder@gmail.com>
2022-03-10 19:45:01 -0500

    Initial files for text-input

--- projects/instant-ui-library/src/lib/input/input.module.ts
+++ projects/instant-ui-library/src/lib/input/input.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextAreaModule } from './text-area/text-area.module';
+import { TextInputModule } from './text-input/text-input.module';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [TextAreaModule],
+  exports: [TextAreaModule, TextInputModule],
 })
 export class InputModule {}
---
+++ projects/instant-ui-library/src/lib/input/text-input/text-input.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TextInputComponent } from './text-input/text-input.component';
+
+@NgModule({
+  declarations: [TextInputComponent],
+  imports: [CommonModule],
+})
+export class TextInputModule {}
